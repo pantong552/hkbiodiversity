45890299ab3e56c375171c792f7cad7ef6157c67
pantong552 <pantong0505.2@gmail.com>
2026-04-24 14:03:28 +0800

    fix vercel error 2

--- src/components/SpeciesCard.tsx
+++ src/components/SpeciesCard.tsx
@@ -141,7 +141,7 @@ export default function SpeciesCard({
 
   return (
     <div 
-      onClick={() => addSpecies(species.taxa_id)} 
+      onClick={() => species.taxa_id && addSpecies(species.taxa_id)} 
       className={`
         group relative bg-white border border-slate-200/50 overflow-hidden shadow-[0_2px_10px_rgba(0,0,0,0.06)]
         hover:shadow-[0_10px_30px_rgba(0,0,0,0.12)] hover:-translate-y-1.5 transition-all duration-500 flex flex-col cursor-pointer
